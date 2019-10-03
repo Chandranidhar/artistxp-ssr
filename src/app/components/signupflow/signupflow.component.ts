bbf1c6e4a91f4d29da0272babf9e1a3208654f02
root <chandrani.influxiq@gmail.com>
2019-10-03 12:13:35 +0530

    change

--- src/app/components/signupflow/signupflow.component.ts
+++ src/app/components/signupflow/signupflow.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signupflow',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupflowComponent implements OnInit {
 
-  constructor() { }
+  isLinear = false;
+  firstFormGroup: FormGroup;s
+  secondFormGroup: FormGroup;
+  constructor(public _formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
 }
